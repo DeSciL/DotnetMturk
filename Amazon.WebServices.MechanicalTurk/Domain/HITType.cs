2a880038850076739a9bae8fca9d380cfd2466f2
Stefan Wehrli <stwehrli@gmail.com>
2015-10-07 17:22:05 +0200

    Added type for HITType - This is a convenience type to allow an easier setup of HITTypes and QualificationRequirements - Update solution files

---
+++ Amazon.WebServices.MechanicalTurk/Domain/HITType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon.WebServices.MechanicalTurk.Domain
+{
+    /// <summary>
+    /// Convenience Type for PowerShell Access
+    /// </summary>
+    public class HITType
+    {
+        /// <remarks />
+        public int AutoApprovalDelayInSeconds { get; set; }
+        /// <remarks />
+        public int AssignmentDurationInSeconds { get; set; }
+        /// <remarks />
+        public Price Reward { get; set; }
+        /// <remarks />
+        public string Title { get; set; }
+        /// <remarks />
+        public string Keywords { get; set; }
+        /// <remarks />
+        public string Description { get; set; }
+        /// <remarks />
+        public QualificationRequirement[] QualificationRequirement { get; set; }
+        /// <remarks />
+        public string[] ResponseGroup { get; set; }
+    }
+}
